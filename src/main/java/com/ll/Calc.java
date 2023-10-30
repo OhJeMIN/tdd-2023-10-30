90853ef480631a0289f721c16575fa149ef40afa
jasonoh22 <jasonoh22@naver.com>
2023-10-30 10:14:39 +0900

    Calc에 run 메서드는 int를 리턴한다.

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,15 +1,7 @@
 package com.ll;
 
 public class Calc {
-    private int answer;
     public static int run(String s){
-        String[] cmd = s.split(" ");
-        int[] arr = new int[cmd.length];
-        for(int i=0;i<cmd.length;i++){
-            if(cmd[i].equals("*")){
-                arr[i+1] = Integer.parseInt(cmd[i-1]) * Integer.parseInt(cmd[i+1]);
-            }
-        }
-        return arr[cmd.length-1];
+        return 0;
     }
 }
